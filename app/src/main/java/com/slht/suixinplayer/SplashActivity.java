2f1a7d7f8bc1941c1d9430ff6ff9cd4f91ff88eb
Lizhineng <512680434@qq.com>
2016-04-13 17:03:34 +0800

    更新

--- app/src/main/java/com/slht/suixinplayer/SplashActivity.java
+++ app/src/main/java/com/slht/suixinplayer/SplashActivity.java
@@ -1,13 +1,13 @@
 package com.slht.suixinplayer;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.widget.Toast;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends Activity {
 
     private static final int START_ACTIVITY = 0x1;
     private Handler handler = new Handler() {
@@ -17,7 +17,8 @@ public class SplashActivity extends AppCompatActivity {
             super.handleMessage(msg);
             if (msg.what == START_ACTIVITY) {
                 Toast.makeText(SplashActivity.this, "this", Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                startActivity(new Intent(SplashActivity.this, com.astuetz.viewpager.extensions.sample.MainActivity
+                        .class));
                 finish();
             }
         }

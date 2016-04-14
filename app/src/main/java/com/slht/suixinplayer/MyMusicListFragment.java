3fbc164361fbf4b5c7f84439317e97f7cc3c2acb
Lizhineng <512680434@qq.com>
2016-04-14 18:18:05 +0800

    优化内存溢出

--- app/src/main/java/com/slht/suixinplayer/MyMusicListFragment.java
+++ app/src/main/java/com/slht/suixinplayer/MyMusicListFragment.java
@@ -6,21 +6,32 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.slht.suixinplayer.Bean.MP3Info;
 import com.slht.suixinplayer.adapter.MyMusicListAdapter;
 import com.slht.suixinplayer.utils.MediaUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
-public class MyMusicListFragment extends Fragment implements AdapterView.OnItemClickListener {
+public class MyMusicListFragment extends Fragment implements AdapterView.OnItemClickListener, View.OnClickListener {
 
     private View parent;
     private MainActivity activity;
     private ListView listview;
     private MyMusicListAdapter adapter;
+
+    private ImageView song_image;
+    private TextView song;
+    private TextView singer;
+    private ImageView pause;
+    private ImageView next;
+
+    private List<MP3Info> mp3Infos;
 
     public static MyMusicListFragment newInstance() {
 //        if (myMusicListFragment == null)
@@ -45,6 +56,7 @@ public class MyMusicListFragment extends Fragment implements AdapterView.OnItemC
 
     private void initEvent() {
         listview.setOnItemClickListener(this);
+        pause.setOnClickListener(this);
     }
 
     @Override
@@ -60,6 +72,8 @@ public class MyMusicListFragment extends Fragment implements AdapterView.OnItemC
                 getActivity().runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
+                        mp3Infos = new ArrayList<MP3Info>();
+                        mp3Infos.addAll(data);
                         adapter = new MyMusicListAdapter(activity, data);
                         listview.setAdapter(adapter);
                     }
@@ -70,6 +84,12 @@ public class MyMusicListFragment extends Fragment implements AdapterView.OnItemC
 
     private void initView() {
         listview = findView(R.id.listview);
+        song_image = findView(R.id.song_image);
+        song = findView(R.id.song);
+        singer = findView(R.id.singer);
+        pause = findView(R.id.pause);
+        next = findView(R.id.next);
+
     }
 
     @Override
@@ -84,7 +104,25 @@ public class MyMusicListFragment extends Fragment implements AdapterView.OnItemC
         activity.playService.paly(position);
     }
 
-    public void changeUiStatus(int position) {
+    public void changeUiStatus(final int position) {
+        activity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                song_image.setImageBitmap(mp3Infos.get(position).getBitImage());
+                song.setText(mp3Infos.get(position).getTitle());
+                singer.setText(mp3Infos.get(position).getArtist());
+                pause.setImageResource(R.mipmap.player_btn_pause_normal);
+            }
+        });
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.pause:
+                activity.playService.pause(pause);
+                break;
+        }
     }
 }
--- app/src/main/java/com/slht/suixinplayer/service/PlayService.java
+++ app/src/main/java/com/slht/suixinplayer/service/PlayService.java
@@ -6,8 +6,10 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Binder;
 import android.os.IBinder;
+import android.widget.ImageView;
 
 import com.slht.suixinplayer.Bean.MP3Info;
+import com.slht.suixinplayer.R;
 import com.slht.suixinplayer.utils.MediaUtils;
 
 import java.io.IOException;
@@ -19,7 +21,7 @@ public class PlayService extends Service {
 
     private MediaPlayer mPlayer;
     private int currentPosition;
-    private List<MP3Info> mp3Infos;
+    private List<MP3Info> mp3Infos = null;
     private MusicUpdateListener musicUpdateListener;
     Runnable updateStatusRunnable = new Runnable() {
         @Override
@@ -54,34 +56,40 @@ public class PlayService extends Service {
      * @param position
      */
     public void paly(final int position) {
-        MediaUtils.getMP3Infos(this, new MediaUtils.QuerySuccess() {
-            @Override
-            public void querySuccess(List<MP3Info> data) {
-                mp3Infos = data;
-                if (position >= 0 && position < mp3Infos.size()) {
-                    try {
-                        mPlayer.reset();
-                        mPlayer.setDataSource(PlayService.this, Uri.parse(data.get(position).getUrl()));
-                        mPlayer.prepare();
-                        mPlayer.start();
-                        currentPosition = position;
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    if (musicUpdateListener != null) {
-                        musicUpdateListener.onChange(currentPosition);
-                    }
+        if (mp3Infos == null) {
+            MediaUtils.getMP3Infos(this, new MediaUtils.QuerySuccess() {
+                @Override
+                public void querySuccess(List<MP3Info> data) {
+                    mp3Infos = data;
                 }
+            });
+        }
+        if (position >= 0 && position < mp3Infos.size()) {
+            try {
+                mPlayer.reset();
+                mPlayer.setDataSource(PlayService.this, Uri.parse(mp3Infos.get(position).getUrl()));
+                mPlayer.prepare();
+                mPlayer.start();
+                currentPosition = position;
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        });
+            if (musicUpdateListener != null) {
+                musicUpdateListener.onChange(currentPosition);
+            }
+        }
     }
 
     /**
      * 暂停
      */
-    public void pause() {
+    public void pause(ImageView imageView) {
         if (mPlayer.isPlaying()) {
             mPlayer.pause();
+            imageView.setImageResource(R.mipmap.player_btn_play_normal);
+        } else {
+            mPlayer.start();
+            imageView.setImageResource(R.mipmap.player_btn_pause_normal);
         }
     }
 
